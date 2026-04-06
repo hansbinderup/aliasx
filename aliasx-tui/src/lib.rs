cfcea00e948f62259e8c18548c2fedccf6d044d7
Hans Binderup <hbinderup94@gmail.com>
2026-04-06 19:33:08 +0200

    tui: generalize fuzzy finding
    Before we had two fuzzy finders. But with the history we actually need a
    new one.
    
    Instead of duplicating the code we can instead generalize it and add
    some properties using the rust "traits".
    
    This is quite a nice handling - but still room for improvements.
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- aliasx-tui/src/lib.rs
+++ aliasx-tui/src/lib.rs
@@ -3,6 +3,6 @@ mod session;
 mod task_fuzzy;
 mod widgets;
 
-pub use fuzzy::{string_fuzzy_finder, string_fuzzy_finder_with};
+pub use fuzzy::{fuzzy_finder, FuzzyConfig, FuzzyList};
 pub use session::TuiSession;
 pub use task_fuzzy::task_fuzzy_finder;
